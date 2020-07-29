b820a97f876517ba7aa685d930793730c12feb06
ANONDOOR <67384556+ANONDOOR@users.noreply.github.com>
2020-07-29 17:20:00 -0500

    Add files via upload

--- anondoor.py
+++ anondoor.py
@@ -98,7 +98,7 @@ def menu():
 
     except KeyboardInterrupt:
         print(Fore.CYAN + "\n\nNos " + Fore.GREEN + "vemos " + Fore.YELLOW +"pronto! :)\n" + Fore.RESET)
-        os.system("rm IP2.txt IP1.txt")
+        os.system("rm IP2.txt IP1.txt IP3.txt")
         sys.exit()
 
 def help():
